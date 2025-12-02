a3827baa28bbe5ba5368c5b45fb2e43de5946fee
maulana <maulanawardana01@gmail.com>
2025-12-02 18:56:01 +0700

    fix: api movie and genre

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\movie;
 
 class MovieController
 {
@@ -11,7 +12,7 @@ class MovieController
      */
     public function index()
     {
-        //
+        return response()->json(movie::all());
     }
 
     /**
@@ -19,15 +20,26 @@ class MovieController
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate(
+            ["title" => "required"],
+            ["author" => "required"],
+            ["writer" => "required"],
+            ["description" => "required"],
+            ["duration" => "required"],
+            ["release_year" => "required"],
+            ["poster_url" => "required"],
+        );
+        $movie = movie::create($data);
+        return response()->json($movie, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+
+    public function show(movie $movie)
     {
-        //
+        return response()->json($movie::all());
     }
 
     /**
@@ -35,14 +47,26 @@ class MovieController
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->validate(
+            ["title" => "required"],
+            ["author" => "required"],
+            ["writer" => "required"],
+            ["description" => "required"],
+            ["duration" => "required"],
+            ["release_year" => "required"],
+            ["poster_url" => "required"],
+        );
+        $movie = movie::findOrFail();
+        $movie = movie::update($data);
+        return response()->json($movie);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(genre $genre)
     {
-        //
+        $movie::delete();
+        return response()->json(["message" => "data dihapus"]);
     }
 }
