4603eb7b4b13a52df97ea8c990901b724521b493
maulana <maulanawardana01@gmail.com>
2025-12-02 19:00:02 +0700

    fix: api movie, genre & controller movie

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API works!']);
 });
 
 Route::apiResource('/genres', GenreController::class);
+Route::apiResource('/movies', MovieController::class);
